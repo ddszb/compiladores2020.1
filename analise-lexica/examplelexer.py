57c406b6ee36cc17ebe9f4a58b2f3fa1a885cb90
danielzambrano <zambranobrasil@gmail.com>
2020-09-22 18:31:27 -0300

    Resolvendo exercício 3.5.1 - Análise Léxica

--- analise-lexica/examplelexer.py
+++ analise-lexica/examplelexer.py
@@ -6,7 +6,8 @@ import ply.lex as lex
 reserved = {
     'if' : 'IF',
     'then' : 'THEN',
-    'else' : 'ELSE'
+    'else' : 'ELSE',
+    'while': 'WHILE'
 }
 
 # List of token names.   This is always required
@@ -19,7 +20,8 @@ tokens = [
     'GT',
     'ID',
     'NUMBER',
-    'RELOP'
+    'RELOP',
+    'STRING'
 ] + list(reserved.values())
 
 # A string containing ignored characters (spaces, tabs and newline)
@@ -32,7 +34,7 @@ def t_LE(t):
     return t
 
 def t_NE(t):
-    r'<>'
+    r'!='
     t.type = 'RELOP'
     t.value = 'NE'
     return t
@@ -56,13 +58,13 @@ def t_GT(t):
     return t
 
 def t_EQ(t):
-    r'='
+    r'=='
     t.type = 'RELOP'
     t.value = 'EQ'
     return t
 
 def t_ID(t):
-    r'[a-zA-Z][a-zA-Z0-9]*'
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value,'ID') # Check for reserved words
     return t
 
@@ -70,6 +72,11 @@ def t_ID(t):
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r'\"([^\\\"]|\\.)*\"'
+    t.type = 'STRING'
     return t
 
 # Error handling rule
