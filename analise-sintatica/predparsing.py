720da156ba1b319a240f86cfdd99f0c6ae94f381
danielzambrano <zambranobrasil@gmail.com>
2020-10-16 19:37:52 -0300

    SlR Parsing table

--- analise-sintatica/predparsing.py
+++ analise-sintatica/predparsing.py
@@ -51,6 +51,7 @@ class Grammar:
         self.pred_parsing_trace = []
         self.pp = pprint.PrettyPrinter()
         self.first_computed = False
+        self.follow_computed = False
     
     def getSymbols(self):
         symbols = self.non_terminals.copy()
@@ -67,9 +68,18 @@ class Grammar:
             for rhs in self.production_rules[s]:
                 self.print_production(s, rhs)
 
+    def first_tab_size(self):
+        return self.tab_size(self.first_tab)
+
     def compute_first(self):
-        for s in self.getSymbols():
-            self.first(s)
+        while True:
+            first_size = self.first_tab_size()
+            for s in self.getSymbols():
+                self.first(s)
+            new_first_size = self.first_tab_size()
+            if first_size == new_first_size:
+                break
+
         self.first_computed = True
             
     def firstW(self, w):
@@ -128,10 +138,7 @@ class Grammar:
                     self.first_log(s, "epsilon", rhs)
                     self.first_tab[s].add("epsilon")
                     break
-                # We must first calculate FIRST(y_1) before
-                # use it.
-                if self.first_tab[y_1] == set():
-                    self.first(y_1)
+
                 # FIRST(y_1) \subseteq FIRST(s)    
                 for a in self.first_tab[y_1]:
                     self.first_log(s, a, rhs)                    
@@ -142,10 +149,7 @@ class Grammar:
                 for i in range(1, len(rhs)):
                     y_ant = rhs[i - 1] # ant =  i - 1
                     y_i = rhs[i]
-                    # We must first calculate FIRST(y_ant) before
-                    # use it.
-                    if self.first_tab[y_ant] == set():
-                        self.first(y_ant)
+
                     # FIRST(y_i) is included in FIRST(s) iff
                     # epsilon \in FISRT(y_j), for every 1 <= j <= i.
                     # However, we only reach j if every k 1 <= k < j
@@ -227,7 +231,7 @@ class Grammar:
             if curr_size == self.follow_tab_size():
                 break
             pass
-
+        self.follow_computed = True
             
     def follow(self, s):
         '''
--- analise-sintatica/slr.py
+++ analise-sintatica/slr.py
@@ -144,24 +144,35 @@ def slr_parsing_table(g):
                 #      and GOTO(Ii, a) = Ij then
                 #        set ACTION[i,􏰥a] =􏰙shift j.
                 #      Here a must be a terminal.􏰋
-
-                # Do your magic here!
-
+                if a in g.terminals:
+                    goto_item = goto(l, a, g)
+                    if goto_item in can:
+                        item = can.index(goto_item)
+                        if item:
+                            action_tab[state_l][a].append(('shift', item))
             else:
-                print("-")
                 # (b) If [A -> alpha *] is in Ii, then set
                 # ACTION[i, a] = "reduce A -> alpha" to
                 # all a in FOLLOW(A); here A may not be S'.
                 # (c) If [S' -> S *] is in Ii, then set
                 # ACTION[i, $] = "accept".
-
-                # Do your magic here!
+                if lhs != g.start_symbol:
+                    follow = g.follow_tab[lhs]
+                    for a in follow:
+                        action_tab[state_l][a].append(('reduce', (lhs, rhs)))
+                else:
+                    action_tab[state_l]['$'].append(('accept',))
 
         # 3. The goto transitions for state i are constructed for all
         # nonterminals A using the rule􏰗 If GOTO(Ii, A) = Ij 􏰉
         # then GOTO[i,􏰥A] = j.􏰋
+        for nt in g.non_terminals:
+            goto_item = goto(l, nt, g)
+            if len(goto_item) and goto_item in can:
+                item = can.index(goto_item)
+                if item:
+                    goto_tab[state_l][nt].append(item)
 
-                # Do your magic here!
 
     return (action_tab, goto_tab)
 
